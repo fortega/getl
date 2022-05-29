0d608ea82981349d7c11b8d9a494da24462e6c2c
Felipe Ortega <felipeortegabustamante@gmail.com>
2022-05-29 12:03:37 -0400

    fix(LoaderConfigAdapter): on empty args return none

--- src/main/scala/com/github/fortega/adapter/LoaderConfigAdapter.scala
+++ src/main/scala/com/github/fortega/adapter/LoaderConfigAdapter.scala
@@ -1,16 +1,21 @@
 package com.github.fortega.adapter
 
 import com.github.fortega.port.ConfigPort
+import org.slf4j.LoggerFactory
 
 object LoaderConfigAdapter {
+  lazy val logger = LoggerFactory.getLogger("LoaderConfig")
+
   private def getFromEnv(
       env: Map[String, String],
       envKey: String = "CONFIG_BASE64"
   ): Option[String] =
     env.get(envKey)
-  private def getFromArgs(cmdArgs: Array[String]): Option[String] = Some(
-    String.join("", cmdArgs: _*)
-  )
+
+  private def getFromArgs(cmdArgs: Array[String]): Option[String] = {
+    val base64 = String.join("", cmdArgs: _*)
+    if (base64.isEmpty) None else Some(base64)
+  }
 
   def apply(
       env: Map[String, String],
